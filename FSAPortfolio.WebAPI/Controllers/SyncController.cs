f9b014029db51df8cb3109041d71e75d99ea4f54
Sam Richards <sam@ipassenger.io>
2020-10-07 08:27:43 +0100

    Sync projects when SyncAll

--- FSAPortfolio.WebAPI/Controllers/SyncController.cs
+++ FSAPortfolio.WebAPI/Controllers/SyncController.cs
@@ -28,6 +28,8 @@ namespace FSAPortfolio.WebAPI.Controllers
             sync.SyncPeople();
 
             sync.SyncPortfolios();
+
+            sync.SyncAllProjects();
             return messages;
         }
 
