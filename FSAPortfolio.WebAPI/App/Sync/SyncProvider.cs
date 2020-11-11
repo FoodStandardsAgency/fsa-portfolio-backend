9f1ef5a8a97c44109487f0fddf47aeb51f33e208
Sam Richards <sam@ipassenger.io>
2020-11-11 15:19:41 +0000

    Add source team value in log for mapping errors

--- FSAPortfolio.WebAPI/App/Sync/SyncProvider.cs
+++ FSAPortfolio.WebAPI/App/Sync/SyncProvider.cs
@@ -513,24 +513,32 @@ namespace FSAPortfolio.WebAPI.App.Sync
 
         private Project MapProject(PortfolioContext dest, IEnumerable<project> sourceProjectDetail, Project destProject, project latestSourceUpdate)
         {
-            //try
-            //{
+            try
+            {
                 mapper.Map(latestSourceUpdate, destProject, opt => opt.Items[nameof(PortfolioContext)] = dest);
                 destProject.Description = sourceProjectDetail.Where(u => !string.IsNullOrEmpty(u.short_desc)).OrderBy(u => u.timestamp).LastOrDefault()?.short_desc; // Take the last description
                 return destProject;
-            //}
-            //catch (AutoMapperMappingException ame)
-            //{
-            //    if (ame.MemberMap.DestinationName == "Size")
-            //    {
-            //        log.Add($"MAPPING ERROR: Source project size = {latestSourceUpdate.project_size}");
-            //    }
-            //    else
-            //    {
-            //        log.Add($"MAPPING ERROR: Destination member = {ame.MemberMap.DestinationName}");
-            //    }
-            //}
-            //return null;
+            }
+            catch (AutoMapperMappingException ame)
+            {
+                if (ame.MemberMap.DestinationName == "Size")
+                {
+                    log.Add($"MAPPING ERROR: Source project size = {latestSourceUpdate.project_size}");
+                }
+                else
+                {
+                    switch(ame.MemberMap.DestinationName)
+                    {
+                        case "Team":
+                            log.Add($"MAPPING ERROR: Destination member = {ame.MemberMap.DestinationName}, source team = {latestSourceUpdate.team}");
+                            break;
+                        default:
+                            log.Add($"MAPPING ERROR: Destination member = {ame.MemberMap.DestinationName}");
+                            break;
+                    }
+                }
+            }
+            return null;
         }
     }
 }
