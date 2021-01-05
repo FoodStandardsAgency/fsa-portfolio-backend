c38155a2049fbaaf0c10f143cd01785356cbe986
Sam Richards <sam@ipassenger.io>
2021-01-05 12:36:59 +0000

    Added sql script to remove unused reservations; updated release notes.

---
+++ FSAPortfolio.Database/Deployment/DeleteUnusedReservations.sql
@@ -0,0 +1,4 @@
+﻿DELETE rv 
+FROM [dbo].[ProjectReservations] rv 
+	LEFT JOIN [dbo].[Projects] pr ON pr.ProjectReservation_Id = rv.Id
+WHERE pr.ProjectReservation_Id IS NULL AND DATEDIFF(day, rv.ReservedAt, GETDATE()) >= 1
