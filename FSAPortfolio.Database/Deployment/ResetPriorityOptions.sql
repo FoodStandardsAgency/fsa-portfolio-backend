9d5a4ddf6e13d11b3d9c32117ed6ccddfd161a05
Sam Richards <sam.richards@food.gov.uk>
2021-01-27 16:21:08 +0000

    DEV2101009: added to priorites reset script; updated release notes

--- FSAPortfolio.Database/Deployment/ResetPriorityOptions.sql
+++ FSAPortfolio.Database/Deployment/ResetPriorityOptions.sql
@@ -1,1 +1,5 @@
 ﻿UPDATE [dbo].[PortfolioLabelConfigs] SET FieldOptions = '0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20' WHERE FieldName = 'priority_main'
+
+UPDATE [dbo].[PortfolioLabelConfigs] SET FieldOptions = '0, 1, 2, 3, 4' 
+WHERE FieldName IN ('funded', 'confidence', 'priorities', 'benefits', 'criticality')
+	AND FieldOptions = ''
