ff2c072bcef41c7480433136b48e85a9b49d4594
Sam Richards <sam.richards@food.gov.uk>
2021-06-24 10:03:22 +0100

    Minor change

--- TestScripts/getPortfolioProjects.sh
+++ TestScripts/getPortfolioProjects.sh
@@ -1,2 +1,2 @@
-curl -s "$apiUrl/Projects?portfolio=$apiPortfolioKey&id=DV2103008" -X GET -H "PowerBIAPIKey: $apiKeyPowerBI" 
-#curl -s "$apiUrl/Projects?portfolio=$apiPortfolioKey&id=DV2103008" -X GET -H "PowerBIAPIKey: $apiKeyPowerBI" > "${outputDir}/${apiPortfolioKey}_projects.json"
+#curl -s "$apiUrl/Projects?portfolio=$apiPortfolioKey&id=DV2103008" -X GET -H "PowerBIAPIKey: $apiKeyPowerBI" 
+curl -s "$apiUrl/Projects?portfolio=$apiPortfolioKey&id=ODD2011001" -X GET -H "PowerBIAPIKey: $apiKeyPowerBI" > "${outputDir}/${apiPortfolioKey}_projects.json"
